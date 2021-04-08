cf44a31a7b9a29be577a5da4b1c081699cc0d5ca
arthur <hauton.arthur@gmail.com>
2021-04-09 00:14:40 +0200

    WaveSO

--- Assets/Scripts/Enemies/WaveSO.cs
+++ Assets/Scripts/Enemies/WaveSO.cs
@@ -4,6 +4,23 @@ namespace Enemies
 {
     public class WaveSO : ScriptableObject
     {
-    
+        [Header("Prefabs")] 
+        public GameObject Orc;
+        public GameObject Ghost;
+        public GameObject Warg;
+        public GameObject Troll;
+        public GameObject Drake;
+        
+        
+        [Header("Wave Settings")]
+        public int NombreOrcs;
+        public int NombreGhost;
+        public int NombreWargs;
+        public int NombreTrolls;
+        public int NombreDrake;
+        public float EndWavePercentage;
+        public float TimeToWait;
+
+
     }
 }
