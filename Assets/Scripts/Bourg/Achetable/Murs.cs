ee09056ebdd6b0fc3854844f38711881e6585114
arthur <hauton.arthur@gmail.com>
2021-05-14 11:12:57 +0200

    Resolve Conflicts

--- Assets/Scripts/Bourg/Achetable/Murs.cs
+++ Assets/Scripts/Bourg/Achetable/Murs.cs
@@ -220,23 +220,14 @@ namespace Assets.Scripts.Bourg.Achetable
             {
                 foreach (Murs wall in _mursAdjacent)
                 {
-<<<<<<< Updated upstream
                     
-                    if (wall.checkedNeighbours.Contains(this))
-                    {
-                        Debug.Log("Modifi les mur adjasents");
-                        wall.checkedNeighbours.Remove(this);
-                        wall.CheckAndUpdate();
-                    }
-=======
                     wall.checkedNeighbours.Remove(this);
                     wall._mursAdjacent.Remove(this);
                     wall.CheckAndUpdate();
->>>>>>> Stashed changes
                 }
             }
             base.OnDestroy();
-           /* if (SecurityValue != 0)
+            if (SecurityValue != 0)
             {
                 Vector2Int[] aura = Playgrid.GetBuildingAura(Playgrid.GetOriginalBuildingCenter(OccupiedCells.ToArray()),
                     (int) Mathf.Sqrt(OccupiedCells.Count), SecurityRange);
@@ -249,7 +240,7 @@ namespace Assets.Scripts.Bourg.Achetable
             foreach (Vector2Int cell in OccupiedCells)
             {
                 Playgrid.GetCell(cell).IndividualMoveValue -= IndividualMoveFactor;
-            }*/
+            }
             Debug.Log("Remove Wall data");
         }
         
