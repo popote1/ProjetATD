5e0c65e084ab78fdc808d2fe3f09b8b16943a10b
popote <yvan.bladet@gmail.com>
2021-05-27 09:45:57 +0200

    fix audioManager

--- Assets/Scripts/Components/MainMenuManagerComponent.cs
+++ Assets/Scripts/Components/MainMenuManagerComponent.cs
@@ -27,6 +27,8 @@ public class MainMenuManagerComponent : MonoBehaviour
     public Sprite PolishFlag;
     public Sprite FrenshFlag;
     public Sprite EnglishFlag;
+    public Slider SliderSound;
+    public Slider SliderMusic;
     
 
     public TMP_InputField InputFieldBorgName;
@@ -40,10 +42,11 @@ public class MainMenuManagerComponent : MonoBehaviour
     public float FadeTime;
 
     [Header(" Changer Panel Sound")] 
+    public AudioClip BackGroundMusic;
     public AudioClip AudioClip;
+    
 
     [Range(0, 1)] public float Volume = 1;
-
     [HideInInspector]public SmoothTerrainLoading SmoothTerrainLoading;
     
     
@@ -57,6 +60,9 @@ public class MainMenuManagerComponent : MonoBehaviour
         BourgName.Desactivat();
         Credit.Desactivat();
         TxtLangue.text = LangueManager.SelectTextData.Tests[1];
+        SliderMusic.value = AudioManager.VolumeMusic;
+        SliderSound.value = AudioManager.VolumeSFX;
+        AudioManager.PlayMusic(BackGroundMusic);
 
     }
 
@@ -139,10 +145,9 @@ public class MainMenuManagerComponent : MonoBehaviour
         MainMenu.Activate();
     }
 
-    public void UIQuit()
-    {
-        Application.Quit();
-    }
+    public void UIOnChangeVolumeMusique() { AudioManager.VolumeMusic = SliderMusic.value; }
+    public void UIOnChangeVolumeSFX() { AudioManager.VolumeSFX = SliderSound.value; }
+    public void UIQuit() { Application.Quit(); }
 
     public void PlaySound() { if (AudioClip != null) AudioManager.PlaySfx(AudioClip, Volume);}
 
